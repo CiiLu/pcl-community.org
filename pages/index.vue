5daf3b3eb42beda696f2b94fc8ea1e11d636533c
Ciilu <109708109+Ciilu@users.noreply.github.com>
2025-06-15 17:59:44 +0800

    HelloWorld:)

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,18 @@
 <template>
     <div>
-        <h1>Hello World!</h1>
+        <h1>Hello World:)</h1>
     </div>
 </template>
 
 <script lang="ts">
 
 </script>
+
+<style>
+    h1 {
+    animation-name: rainbow;
+    animation-duration: 5s;
+    animation-timing-function: linear;
+    animation-iteration-count: infinite;
+    }
+</style>
