8b5142f936d23a6ebe340c7cd08f1dd908a420dc
Ciilu <109708109+Ciilu@users.noreply.github.com>
2025-06-15 18:03:31 +0800

    HelloWorld:(

--- pages/index.vue
+++ pages/index.vue
@@ -9,10 +9,20 @@
 </script>
 
 <style>
-    h1 {
-    animation-name: rainbow;
-    animation-duration: 5s;
-    animation-timing-function: linear;
-    animation-iteration-count: infinite;
-    }
+      h1 {
+        animation-name: rainbow;
+        animation-duration: 3s;
+        animation-timing-function: linear;
+        animation-iteration-count: infinite;
+      }
+
+      @keyframes rainbow {
+        0% { color: #ff0000; }
+        17% { color: #ffff00; }
+        33% { color: #00ff00; }
+        50% { color: #00ffff; }
+        67% { color: #0000ff; }
+        83% { color: #ff00ff; }
+        100% { color: #ff0000; }
+      }
 </style>
